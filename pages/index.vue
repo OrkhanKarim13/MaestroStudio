e3062aca83d46c86460e3ac2425037a864fa8924
Asgarov Asgar <asgara@maestro.st>
2024-01-24 12:16:08 +0400

    added home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,193 @@
 <template>
-  <div>
-    Welcomeee
+  <section class="bg-[#0047FF] md:mx-9 md:mb-6 md:rounded-md">
+    <div class="flex flex-col items-center lg:items-start justfy-center px-8 py-14 md:p-12  gap-9">
+      <h1
+        class="text-5xl leading-snug lg:text-7xl text-center lg:text-left font-normal lg:leading-snug text-white w-full lg:w-3/5">
+        BUILDING INNOVATIVE FOR INDUSTRY LEADERS</h1>
+      <a class="w-full md:w-auto text-center md:text-center py-4 px-8 bg-white text-black font-semibold rounded-full uppercase hover:bg-[#fdfdfd] hover:scale-[1.01]"
+        href="/">To contact page</a>
+    </div>
+  </section>
+  <section class=" md:mx-9 md:mb-6  ">
+    <div class=" grid grid-cols-1 lg:grid-cols-2 lg:gap-6">
+      <div class="lg:rounded-md"> <img class="w-full h-full" src="../assets/gif/home.gif" alt="image"></div>
+      <div
+        class="  bg-[#0047FF]  pt-6 px-4 pb-12 lg:p-12 flex flex-col items-start justify-between lg:rounded-md gap-20 ">
+        <div class=" w-4/5 text-white text-4xl font-bold leading-11">The website we did for Fish Restaurant
+        </div>
+        <div class=" flex items-center justify-center "><img src="../assets/images/Arrow 3.svg" alt="arrow">
+        </div>
+        <div class=" flex flex-col items-start justify-center gap-5 ">
+          <p class="text-white text-lg w-full lg:w-2/3">A secure messaging app for professional networks, companies and
+            organizations.</p>
+          <a class="w-full md:w-auto text-center md:text-center py-4 px-8 bg-white text-black font-semibold rounded-full hover:bg-[#fdfdfd] hover:scale-[1.01]"
+            href="/">View</a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="my-6 md:mx-9 md:mb-6 bg-[#0047FF] md:rounded-md p-4 lg:p-12">
+    <div class="flex flex-col items-center justify-center gap-8 lg:gap-12 ">
+      <h2 class="text-white text-xl text-center lg:text-3xl font-bold leading-6">Services we provide</h2>
+      <div class="w-full bg-white rounded-md grid  grid-cols-1  md:grid-cols-2 lg:gap-40">
+        <div class=" p-3 lg:p-6">
+          <ul class="flex flex-col items-start justify-center text-lg lg:text-2xl font-semibold">
+            <li class="px-4 py-2 lg:px-8 lg:py-4 font-bold">Web Development
+            </li>
+            <li class="px-4 py-2 lg:px-8 lg:py-4 font-bold">Mobile Development
+            </li>
+            <li class="px-4 py-2 lg:px-8 lg:py-4 font-bold">Custom Software Development</li>
+            <li class="px-4 py-2 lg:px-8 lg:py-4 font-bold">UI/UX Design</li>
+            <li class="px-4 py-2 lg:px-8 lg:py-4 font-bold">Testing & Quality Assurance</li>
+            <li class="px-4 py-2 lg:px-8 lg:py-4 font-bold">AI Software Development</li>
+            <li class="px-4 py-2 lg:px-8 lg:py-4 font-bold"><a
+                class="uppercase  text-blue-700 text-lg font-semibold cursor-pointer" href="/">View</a></li>
+          </ul>
+        </div>
+        <div class=" p-6">
+          <img src="../assets/images/8.svg" alt="image">
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="md:mx-9 md:mb-6 bg-[#0047FF] px-4 py-6 lg:p-12 md:rounded-md">
+    <div class="flex flex-col gap-10">
+      <div class="w-full grid grid-cols-1 md:grid-cols-5 items-center justify-between gap-6">
+        <h2 class="md:col-span-4  text-white text-3xl font-bold leading-6 text-center md:text-left">Projects we provide
+        </h2>
+        <a class="col-span-1 uppercase cursor-pointer  px-8 py-5 rounded-full bg-transparent border border-white  text-white font-semibold text-center "
+          href="#">view all</a>
+      </div>
+
+      <div class=" grid grid-cols-1 md:grid-cols-2 lg:gap-6 gap-8">
+        <div class="rounded-md bg-white px-4 py-6 md:p-12 flex flex-col items-start justify-center  gap-6">
+          <img class="w-full mx-auto" src="../assets/images/Frame 12.svg" alt="image">
+          <div class="flex flex-col items-start justify-center mx-auto gap-3">
+            <h4 class="text-2xl text-black font-bold">EVOLVING ONE OF THE WORLD'S
+              MOST ICONIC FOOTBALL CLUBS</h4>
+            <p class="text-black text-lg">When we started in 2010, we were just a tight group dedicated to the craft of
+              product building. Our attitude hasn't changed—but since then, we’ve grown to be a team.</p>
+            <a class="py-4 px-8 border border-black rounded-full text-black font-semibold rounded-ful uppercase hover:bg-[#fdfdfd] hover:scale-[1.01]"
+              href="/">view All</a>
+          </div>
+        </div>
+        <div class="rounded-md bg-white px-4 py-6 md:p-12 flex flex-col items-start justify-center gap-6">
+          <img class="w-full mx-auto" src="../assets/images/Frame 121.svg" alt="image">
+          <div class="flex flex-col items-start justify-center  mx-auto gap-3">
+            <h4 class="text-2xl text-black font-bold">THE BEST THINGS IN
+              LIFE HAPPEN AROUND THE TABLE</h4>
+            <p class="text-black text-lg">When we started in 2010, we were just a tight group dedicated to the craft of
+              product building. Our attitude hasn't changed—but since then, we’ve grown to be a team.</p>
+            <a class="py-4 px-8 border border-black rounded-full text-black font-semibold rounded-ful uppercase hover:bg-[#fdfdfd] hover:scale-[1.01]"
+              href="/">view All</a>
+          </div>
+        </div>
+        <div class="rounded-md bg-white px-4 py-6 md:p-12 flex flex-col items-start justify-center gap-6">
+          <img class="w-full mx-auto" src="../assets/images/Frame 122.svg" alt="image">
+          <div class="flex flex-col items-start justify-center  mx-auto gap-3">
+            <h4 class="text-2xl text-black font-bold">UNITING THE WORLD'S BIGGEST
+              PUBLISHER BEHIND A SHARED MISSION</h4>
+            <p class="text-black text-lg">When we started in 2010, we were just a tight group dedicated to the craft of
+              product building. Our attitude hasn't changed—but since then, we’ve grown to be a team.</p>
+            <a class="py-4 px-8 border border-black rounded-full text-black font-semibold rounded-ful uppercase hover:bg-[#fdfdfd] hover:scale-[1.01]"
+              href="/">view All</a>
+          </div>
+        </div>
+        <div class="rounded-md bg-white px-4 py-6 md:p-12 flex flex-col items-start justify-center gap-6">
+          <img class="w-full mx-auto" src="../assets/images/Frame 12 (3).svg" alt="image">
+          <div class="flex flex-col items-start justify-center  mx-auto gap-3">
+            <h4 class="text-2xl text-black font-bold">UNITING THE WORLD'S BIGGEST
+              PUBLISHER BEHIND A SHARED MISSION</h4>
+            <p class="text-black text-lg">When we started in 2010, we were just a tight group dedicated to the craft of
+              product building. Our attitude hasn't changed—but since then, we’ve grown to be a team.</p>
+            <a class="py-4 px-8 border border-black rounded-full text-black font-semibold rounded-ful uppercase hover:bg-[#fdfdfd] hover:scale-[1.01]"
+              href="/">view All</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </section>
+  <PartnerList />
+  <section class="md:mx-9 my-6 md:mb-6 bg-[#0047FF] px-4 py-3 md:p-12 md:rounded-md">
+    <h3 class=" text-white text-3xl font-bold leading-10 text-center md:text-left">Blog on Software Development</h3>
+
+    <div class="py-12 w-full grid grid-cols-1 lg:grid-cols-2 items-center gap-5">
+      <div class=" flex flex-col items-center justify-center rounded-md">
+        <div class="w-full flex flex-col items-start justify-center bg-white rounded-t-md  p-6 gap-6">
+          <div class="flex items-center justify-start gap-6">
+            <span>Dec 01, 2023</span> <span>8 min read</span>
+          </div>
+          <div>
+            <h4 class="text-xl lg:text-3xl font-bold leading-normal">EVOLVING ONE OF THE WORLD’S MOST ICONIC FOOTBALL
+              CLUBS
+            </h4>
+          </div>
+          <div class="flex items-center gap-4">
+            <img src="../assets/images/Ellipse 1 (1).svg" alt="person">
+            <p class="text-base font-normal leading-normal">Anastasiia Chornous</p>
+          </div>
+
+        </div>
+        <div class="w-full object-cover">
+          <img class="w-full " src="../assets/images/Card-cover.svg" alt="image">
+        </div>
+        <div class="w-full px-6 py-4 flex items-center justify-between rounded-b-md  bg-white">
+          <a class="uppercase md:text-lg  text-md font-semibold cursor-pointer" href="/">view All</a>
+          <img src="../assets/images/Arrow 1.svg" alt="">
+        </div>
+      </div>
+      <div class=" flex flex-col items-center justify-center rounded-md">
+        <div class="w-full flex flex-col items-start justify-center bg-white rounded-t-md  p-6 gap-6">
+          <div class="flex items-center justify-start gap-6">
+            <span>Dec 01, 2023</span> <span>8 min read</span>
+          </div>
+          <div>
+            <h4 class="text-xl lg:text-3xl font-bold leading-normal">EVOLVING ONE OF THE WORLD’S MOST ICONIC FOOTBALL
+              CLUBS
+            </h4>
+          </div>
+          <div class="flex items-center gap-4">
+            <img src="../assets/images/Ellipse 1 (1).svg" alt="person">
+            <p class="text-base font-normal leading-normal">Anastasiia Chornous</p>
+          </div>
+
+        </div>
+        <div class="w-full object-cover">
+          <img class="w-full " src="../assets/images/Card-cover.svg" alt="image">
+        </div>
+        <div class="w-full px-6 py-4 flex items-center justify-between rounded-b-md  bg-white">
+          <a class="uppercase text-lg font-semibold cursor-pointer" href="/">view All</a>
+          <img src="../assets/images/Arrow 1.svg" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section>
+  <section class=" md:mx-9 md:mb-6 mx-4 my-6 rounded-md flex flex-col gap-6">
+    <div class="w-ful grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 items-center gap-6">
+      <div class="col-span-1 px-8 py-12 bg-[#0047FF] rounded-md flex flex-col items-center justify-center">
+        <h3 class="text-4xl text-white font-bold " style="line-height: 70px;">390+</h3><img
+          src="../assets/images/Line 1.svg" class="mb-4" alt="line">
+        <p class="text-white text-base font-bold leading-none">PROJECTS</p>
+      </div>
+      <div class="col-span-1 px-8 py-12 bg-[#0047FF] rounded-md flex flex-col items-center justify-center">
+        <h3 class="text-4xl text-white font-bold " style="line-height: 70px;">235+</h3><img
+          src="../assets/images/Line 1.svg" class="mb-4" alt="line">
+        <p class="text-white text-base font-bold leading-none">CLIENTS</p>
+      </div>
+      <div class="col-span-1 px-8 py-12 bg-[#0047FF] rounded-md flex flex-col items-center justify-center">
+        <h3 class="text-4xl text-white font-bold " style="line-height: 70px;">200+</h3><img
+          src="../assets/images/Line 1.svg" class="mb-4" alt="line">
+        <p class="text-white text-base font-bold leading-none">TEAM SIZE</p>
+      </div>
+      <div class="col-span-1 px-8 py-12 bg-[#0047FF] rounded-md flex flex-col items-center justify-center">
+      <h3 class="text-4xl text-white font-bold " style="line-height: 70px;">12+</h3><img
+        src="../assets/images/Line 1.svg" class="mb-4" alt="line">
+      <p class="text-white text-base font-bold leading-none">YEARS ON THE MARKET</p>
+    </div>
   </div>
-</template>
+
+
+</section>
+<!-- <ContactForm/> --></template>
