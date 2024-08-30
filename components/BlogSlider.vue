450780cbbdeb0bc44c600a92e73fd326ded3480c
OrkhanKarim13 <mrorkhankarimli@gmail.com>
2024-08-30 17:27:26 +0400

    change

--- components/BlogSlider.vue
+++ components/BlogSlider.vue
@@ -1,48 +1,48 @@
 <template>
-    <Swiper class="mySwiper" :spaceBetween="20" :slidesPerView="1" :breakpoints="breakpoints">
-        <SwiperSlide v-for="blog in blogs" :key="blog.id">
-            <div class="border flex flex-col items-center justify-center rounded-md">
-                <div class="w-full flex flex-col items-start justify-center bg-white rounded-t-md p-6 gap-0 md:gap-6">
+    <swiper class="mySwiper" space-between="20" :slidesPerView="1" :breakpoints="{
+        
+    }">
+        <swiper-slide v-for="blog in blogs" :key="blog.id" :blog="blog"
+            class="w-full flex justify-center py-6 items-center h-64">
+            <div class="border  flex flex-col items-center justify-center rounded-md">
+                <div class="w-full flex flex-col items-start justify-center bg-white rounded-t-md  p-6 gap-0 md:gap-6">
                     <div class="h-[70px]">
-                        <h4 class="text-xl lg:text-3xl text-black font-bold leading-normal">{{ blog.title }}</h4>
+                        <h4 class="text-xl lg:text-3xl text-black font-bold leading-normal">{{ blog?.title }}
+                        </h4>
                     </div>
-                    <span class="text-black">{{ blog.posted_date }}</span>
+                    <span class="text-black">{{ blog?.posted_date }}</span>
                 </div>
-                <div class="w-full">
-                    <img class="border object-cover" :src="blog.cover_img" alt="image">
+                <div class="w-full   ">
+                    <img class="border object-cover" :src="blog?.cover_img" alt="image">
                 </div>
-                <div @click="() => goToDetails(blog.id)"
-                    class="w-full px-6 py-4 flex cursor-pointer z-10 items-center justify-between rounded-b-md bg-white">
-                    <button class="uppercase md:text-lg text-md font-semibold cursor-pointer text-black hover:scale-[1.01]">
-                        Ətraflı bax
-                    </button>
+                <div 
+                    class="w-full px-6 py-4 flex cursor-pointer z-10 items-center justify-between rounded-b-md  bg-white">
+
+                    <button
+                        class="uppercase md:text-lg  text-md font-semibold cursor-pointer text-black  hover:scale-[1.01]">Ətraflı
+                        bax</button>
                     <img src="~/assets/images/arrow-right.svg" alt="arrow">
                 </div>
             </div>
-        </SwiperSlide>
-    </Swiper>
+        </swiper-slide>
+
+    </swiper>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue';
-import { useRouter } from '#app'; // Nuxt 3 kullanımı
+<script setup>
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import 'swiper/swiper-bundle.css';
-import getBlogs from '@/public/blogs.json';
 
-const blogs = ref(getBlogs);
+import getBlogs from "@/public/blogs.json"
+
+const blogs = ref(getBlogs)
 const router = useRouter();
 
-const breakpoints = {
-    768: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-    },
-};
 
-const goToDetails = (id: string | number) => {
-    router.push(`/blogs/${id}`);
-};
+
+// const goToDetails = (id: string | number) => {
+//     router.push(`./blogs/${id}`);
+// };
 </script>
 
 <style scoped>
