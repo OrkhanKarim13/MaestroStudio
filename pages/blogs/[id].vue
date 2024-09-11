6a8d8198fbd59ba4c079d4d94031c826de9af203
OrkhanKarim13 <mrorkhankarimli@gmail.com>
2024-09-11 16:59:52 +0400

    Blog page responsived

--- pages/blogs/[id].vue
+++ pages/blogs/[id].vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="blog">
-    <section class="mx-9 mb-6 bg-[#0047FF]  rounded-md">
+    <section class="md:mx-9 md:mb-6 bg-[#0047FF]  md:rounded-md">
       <div class="flex flex-col items-start justify-center">
         <div class="flex flex-col items-center justify-center p-6">
           <div class="w-full flex items-center gap-6 text-white">
@@ -8,22 +8,22 @@
             
           </div>
           <div class="w-full">
-            <h2 data-aos="fade-left" class="text-6xl w-3/4 text-white leading-snug">{{ blog?.title[locale] }}</h2>
+            <h2 data-aos="fade-left" class="md:text-6xl text-[40px] md:w-3/4 text-white leading-snug">{{ blog?.title[locale] }}</h2>
 
           </div>
         </div>
-        <div class="w-full h-[500px]"><img class="block md:hidden object-cover w-full"
+        <div class="w-full md:h-[500px]"><img class="block md:hidden object-cover w-full"
             src="~/assets/images/Frame1222.svg" alt="cover">
           <video class="hidden md:block object-cover h-full w-full" :src="blog?.cover_video" loop autoplay
             muted></video>
         </div>
-        <div class="py-12 w-full text-center">
-          <p class="text-white  text-2xl text-justify w-2/3 mx-auto">{{ blog?.description[locale] }}</p>
+        <div class="md:py-12 w-full text-center">
+          <p class="text-white  text-2xl text-justify p-6 md:w-2/3 mx-auto">{{ blog?.description[locale] }}</p>
         </div>
-        <div class=" w-2/3 mx-auto h-[450px] flex items-center justify-center"><img class="object-cover w-full h-full "
+        <div class=" w-2/3 mx-auto md:h-[450px] flex items-center justify-center"><img class="object-cover w-full h-full "
             :src="blog?.cover_img" alt="cover"></div>
         <div class="py-12 w-full text-center">
-          <div class="text-white text-2xl text-justify flex flex-col items-start justify-center gap-4 w-2/3 mx-auto ">
+          <div class="text-white text-2xl text-justify flex flex-col items-start justify-center p-6 gap-4 md:w-2/3 md:mx-auto ">
             {{ blog?.content[locale] }}
           </div>
         </div>
