6c97fd91e5aef8e72207251f76638b78bbdbf958
OrkhanKarim13 <mrorkhankarimli@gmail.com>
2024-09-12 13:58:12 +0400

    icon added

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -53,16 +53,16 @@
       <!-- Custom language dropdown -->
       <div class="relative  flex gap-4 uppercase text-black px-8 py-4">
         <div
-          class="cursor-pointer border  md:border md:border-blue-500 md:rounded-full px-4 py-2"
+          class="cursor-pointer border flex gap-2  md:border md:border-blue-500 md:rounded-full px-4 py-2"
           @click="toggleDropdown"
         >
-          {{ languages.find(lang => lang.code === selectedLanguage).label }}
+          {{ languages.find(lang => lang.code === selectedLanguage).label }} <img src="../public/images/Frame.svg" alt="">
         </div>
 
         <!-- Dropdown content -->
         <ul
           v-if="isDropdownOpen"
-          class="absolute top-14 bg-white border border-gray-300 rounded-lg mt-2 "
+          class="absolute top-14 w-[80px] bg-white border border-gray-300 rounded-lg mt-2 "
         >
           <li
             v-for="lang in languages"
